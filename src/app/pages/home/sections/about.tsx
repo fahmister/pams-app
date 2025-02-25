37cfb2f143e812a53be8815319214d1b98a47222
fahmister <fahmisimister@gmail.com>
2025-02-26 01:40:42 +0700

    Tambah Fitur About, Lanjut Kembangkan di Kantor

---
+++ src/app/pages/home/sections/about.tsx
@@ -0,0 +1,4 @@
+import "@/app/pages/home/homestyle.css"
+import Image from "next/image";
+export default function HomeView() {
+  return (
--- src/app/pages/home/sections/hero.tsx
+++ src/app/pages/home/sections/hero.tsx
@@ -8,7 +8,7 @@ export default function HomeView() {
         <h1 className="hero-title">Full Stack Developer</h1>
         <button className="hero-button">Contact Me</button>
       </div>
-      <Image src="/Fahmi.jpg" alt="Profile" className="hero-image" 
+      <Image src="/Fahmi.jpg" alt="Profile" className="rounded-2xl object-cover mx-auto"
       width={600} height={736}
       />
     </div>
